fbeb8fbb2a611abd2340cd44171a4c6cbd67071b
sayedsajidali <sayed.ali@affiliate.imd.org>
2021-11-17 22:23:09 +0530

    Part 3: Failing test case

--- Restaurant_project -Student version/src/test/java/RestaurantTest.java
+++ Restaurant_project -Student version/src/test/java/RestaurantTest.java
@@ -1,5 +1,5 @@
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;  
+import org.junit.jupiter.api.Test; 
 import org.mockito.Mockito;
 
 import java.time.LocalTime;
